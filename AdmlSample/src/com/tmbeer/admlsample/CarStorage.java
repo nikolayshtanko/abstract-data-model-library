5b1d7aa3171a35765fd8af866893ae86927617c4
Nikolay Shtanko <shtankonickolay@gmail.com>
2013-04-11 02:18:53 +0400

    test Stat
    test Stat

--- AdmlSample/src/com/tmbeer/admlsample/CarStorage.java
+++ AdmlSample/src/com/tmbeer/admlsample/CarStorage.java
@@ -1,7 +1,15 @@
 package com.tmbeer.admlsample;
 
 import com.tmbeer.admlsdk.storage.AbstractStorage;
+import com.tmbeer.admlsdk.utils.Stat;
 
 public class CarStorage extends AbstractStorage<Integer, CarData> {
+	
+	public CarStorage() {
+		Stat stat = new Stat(this);
+		stat.inc();
+		stat.inc();
+		stat.complete();
+	}
 
 }
